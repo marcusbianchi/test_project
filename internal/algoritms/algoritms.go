bc61c8cbadd058618d19fa17cfdd205c15bb0dd9
Marcus Vinicius Bianchi Dos Santos <marcus.bianchi@ifood.com.br>
2021-08-26 08:57:02 -0300

    interview

--- internal/algoritms/algoritms.go
+++ internal/algoritms/algoritms.go
@@ -1,5 +1,137 @@
 package algoritms
 
+import "math"
+
+var counter = 0
+
+type GiftCard struct {
+	// customer string //Nil
+	Id               int
+	Brand            string
+	Face_value       int
+	Discounted_value int //Customer payed value
+}
+
+type Basket struct {
+	Customer_id   int
+	Gift_card_ids []int //Customer payed value
+}
+
+func (b Basket) AddGiftToBasket(gift_card_id int) Basket {
+	//validations
+	b.Gift_card_ids = append(b.Gift_card_ids, gift_card_id)
+	return b
+}
+
+//The system should be demo-able via unit tests (preferably) or the console.
+//The system does not require persistence.
+//The system should support more than one customer.
+//There should be some default gift cards which you can create using static data.
+//Multiple customers should be able to add multiple types of gift cards to their basket.
+//Gift cards have a face value, and a price which is discounted by the brand. The customer pays the discounted price.
+//The discount, and therefore the price of a gift card, can be changed at any time.
+//Provide a mechanism to calculate a customer’s basket total price. Customers’ basket values should reflect the updated prices.
+
 func Hello(name string) string {
 	return "Hello " + name
 }
+
+func NewBasket(customer_id int, gift_card_id int) Basket {
+	//validations
+	return Basket{
+		Customer_id:   customer_id,
+		Gift_card_ids: []int{gift_card_id},
+	}
+}
+
+func NewGiftCard(brand string, face_value int, discount_percentage int) GiftCard {
+	id := counter
+	counter += 1
+	discounted := CalculateDiscountValue(discount_percentage, face_value)
+	return GiftCard{
+		Id:               id,
+		Brand:            brand,
+		Face_value:       face_value,
+		Discounted_value: discounted,
+	}
+}
+
+func CalculateDiscountValue(discount_percentage int, face_value int) int {
+	float_value := float64(discount_percentage) / 10000
+	discounted := float64(face_value) * (1 - float_value)
+	return int(math.Floor(discounted))
+}
+
+func AddGiftToBasket(basket Basket, gift_card_id int) Basket {
+	//validations
+	basket.Gift_card_ids = append(basket.Gift_card_ids, gift_card_id)
+	return basket
+}
+
+func UpdateGiftCardValue(giftCard GiftCard, face_value int, discount_percentage int) GiftCard {
+	discounted := CalculateDiscountValue(discount_percentage, face_value)
+	giftCard.Discounted_value = discounted
+	giftCard.Face_value = face_value
+	return giftCard
+}
+
+//func CalculateBasketValue(giftCards map[int]GiftCard, basket Basket) int {
+//	for i := range collection {
+//
+//	}
+//}
+
+//func NewGiftCardAmazon5Pound() GiftCard {
+//	id := counter
+//	counter += 1
+//	return GiftCard{
+//		id:               id,
+//		brand:            "amazon",
+//		face_value:       1000,
+//		discounted_value: 950,
+//	}
+//}
+//
+//func NewGiftCardAmazon10Pound() GiftCard {
+//	id := counter
+//	counter += 1
+//	return GiftCard{
+//		id:               id,
+//		brand:            "amazon",
+//		face_value:       500,
+//		discounted_value: 475,
+//	}
+//}
+//
+//func NewGiftCardStarbucks5Pound() GiftCard {
+//	id := counter
+//	counter += 1
+//	return GiftCard{
+//		id:               id,
+//		brand:            "starbucks",
+//		face_value:       1000,
+//		discounted_value: 900,
+//	}
+//}
+//
+//func NewGiftCardStarbucks10Pound() GiftCard {
+//	id := counter
+//	counter += 1
+//	return GiftCard{
+//		id:               id,
+//		brand:            "starbucks",
+//		face_value:       500,
+//		discounted_value: 450,
+//	}
+//}
+
+//employeeSalary := make(map[string]int)
+//p := []string{"a", "b", "c"}
+//p = append(p, "d")
+//s := make([]string, 2, 3)
+//for k, v := range myMap {
+//	keys = append(keys, k)
+//	values = append(values, v)
+//}
+
+// GitftCards [1: GiftCard]
--- internal/algoritms/algoritms_test.go
+++ internal/algoritms/algoritms_test.go
@@ -10,3 +10,36 @@ func TestHelloWorld(t *testing.T) {
 	actual := algoritms.Hello("Marcus")
 	assert.Equal(t, "Hello Marcus", actual)
 }
+
+func TestNewGiftCard(t *testing.T) {
+	actual := algoritms.NewGiftCard("amazon", 500, 500)
+	assert.Equal(t, "amazon", actual.Brand)
+	assert.Equal(t, 0, actual.Id)
+	assert.Equal(t, 475, actual.Discounted_value)
+	assert.Equal(t, 500, actual.Face_value)
+}
+
+func TestBasket(t *testing.T) {
+	actual := algoritms.NewBasket(1, 0)
+	assert.Equal(t, 1, actual.Customer_id)
+	assert.Equal(t, 0, actual.Gift_card_ids[0])
+	assert.Equal(t, 1, len(actual.Gift_card_ids))
+}
+
+func TestAddGiftToBasket(t *testing.T) {
+	basket := algoritms.NewBasket(1, 0)
+	actual := algoritms.AddGiftToBasket(basket, 1)
+	assert.Equal(t, 1, actual.Customer_id)
+	assert.Equal(t, 0, actual.Gift_card_ids[0])
+	assert.Equal(t, 1, actual.Gift_card_ids[1])
+	assert.Equal(t, 2, len(actual.Gift_card_ids))
+}
+
+func TestUpdateGiftCardValue(t *testing.T) {
+	giftCard := algoritms.NewGiftCard("amazon", 500, 500)
+	actual := algoritms.UpdateGiftCardValue(giftCard, 1000, 1000)
+	assert.Equal(t, "amazon", actual.Brand)
+	assert.Equal(t, 0, actual.Id)
+	assert.Equal(t, 900, actual.Discounted_value)
+	assert.Equal(t, 1000, actual.Face_value)
+}
